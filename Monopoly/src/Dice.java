c7f44930ac4d9a5bdc2353a104ed90dda9b27782
DDDuy1732 <168977381+DDDuy1732@users.noreply.github.com>
2024-05-30 13:50:40 +0700

    Update Dice.java

--- Monopoly/src/Dice.java
+++ Monopoly/src/Dice.java
@@ -1,4 +1,19 @@
-public class Dice {
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class Dice extends JPanel{
+
+    private ImageIcon[] diceFaces = {
+            new ImageIcon("dice1.png"),
+            new ImageIcon("dice2.png"),
+            new ImageIcon("dice3.png"),
+            new ImageIcon("dice4.png"),
+            new ImageIcon("dice5.png"),
+            new ImageIcon("dice6.png")
+    };
+    private ImageIcon currentFace;
+
     private int valueDice;
 
     public int getValueDice() {
@@ -9,5 +24,21 @@ public class Dice {
         valueDice = 1 + (int) (Math.random() * 6);
         return valueDice;
     }
+public Dice() {
+        currentFace = diceFaces[0];
+        valueDice = 1;
+       
+    }
 
+    public void rollDice() {
+        valueDice = (valueDice % 6) + 1;
+        currentFace = diceFaces[valueDice - 1];
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        currentFace.paintIcon(this, g, 30, 30);
+    }
 }
