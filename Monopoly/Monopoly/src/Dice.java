dc2b788eda1ca57285c6bbcdc4e527c802358ac3
DDDuy1732 <168977381+DDDuy1732@users.noreply.github.com>
2024-06-05 20:17:49 +0700

    Update Dice.java

--- Monopoly/Monopoly/src/Dice.java
+++ Monopoly/Monopoly/src/Dice.java
@@ -1,44 +1,41 @@
+package src;
+
+    import java.awt.image.BufferedImage;
+    import javax.imageio.*;
+    import java.io.File;
+    import java.io.IOException;
+
 import javax.swing.ImageIcon;
-import javax.swing.JPanel;
-import java.awt.*;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
 
-public class Dice extends JPanel{
+public class DiceRoll extends JComponent {
 
-    private ImageIcon[] diceFaces = {
-            new ImageIcon("Monopoly\\\\src\\\\ImageIcon\\\\dice1.png"),
-            new ImageIcon("Monopoly\\\\src\\\\ImageIcon\\\\dice2.png"),
-            new ImageIcon("Monopoly\\\\src\\\\ImageIcon\\\\dice3.png"),
-            new ImageIcon("Monopoly\\\\src\\\\ImageIcon\\\\dice4.png"),
-            new ImageIcon("Monopoly\\\\src\\\\ImageIcon\\\\dice5.png"),
-            new ImageIcon("Monopoly\\\\src\\\\ImageIcon\\\\dice6.png")
-    };
-    private ImageIcon currentFace;
+private BufferedImage dice1;
+private BufferedImage dice2;
+private BufferedImage dice3;
+private BufferedImage dice4;
+private BufferedImage dice5;
+private BufferedImage dice6;
 
-    private int valueDice;
 
-    public int getValueDice() {
-        return valueDice;
+public DiceRoll()
+{
+    try {
+        dice1 = (ImageIO.read(new File("src/ImageIcon/dice1.png")));
+        dice2 = ImageIO.read(new File("src/ImageIcon/dice2.png"));
+        dice3 = ImageIO.read(new File("src/ImageIcon/dice3.png"));
+        dice4 = ImageIO.read(new File("src/ImageIcon/dice4.png"));
+        dice5 = ImageIO.read(new File("src/ImageIcon/dice5.png"));
+        dice6 = ImageIO.read(new File("src/ImageIcon/dice6.png"));
+    } catch (IOException ex){
+        System.err.println("That is invalid");
     }
+}
 
-    public int roll() {
-        valueDice = 1 + (int) (Math.random() * 6);
-        return valueDice;
-    }
-public Dice() {
-        currentFace = diceFaces[0];
-        valueDice = 1;
-       
-    }
 
-    public void rollDice() {
-        valueDice = (valueDice % 6) + 1;
-        currentFace = diceFaces[valueDice - 1];
-        repaint();
-    }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        currentFace.paintIcon(this, g, 30, 30);
+public void roll(JLabel diceLabel) {
+    int diceResult = (int)(5 * Math.random() + 1);
+    diceLabel.setIcon(new ImageIcon("src/ImageIcon/dice" + diceResult + ".png"));
     }
 }
