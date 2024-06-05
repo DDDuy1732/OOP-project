fad94395d12de3f37a49a0fbadbe4f872856fa51
Qu0cThai <quocthai07112003@gmail.com>
2024-06-05 18:51:24 +0700

    Update Monopoly.java

--- Monopoly/Monopoly/src/Monopoly.java
+++ Monopoly/Monopoly/src/Monopoly.java
@@ -89,22 +89,20 @@ public class Monopoly extends JPanel {
     }
 
     public void runRound(Board board, GUIV2 gui) {
-
         Player currentPlayer = playerQueue.poll();
-
+    
         if (!currentPlayer.isInJail()) {
             takeTurn(currentPlayer, board, gui);
         } else if (currentPlayer.isInJail()) {
             currentPlayer.handleJailInteraction(board, gui);
         }
-
+    
         playerQueue.offer(currentPlayer);
         if (currentPlayer.isBankrupt()) {
             System.out.println("Player " + currentPlayer.getName() + " is bankrupt!!!");
             removePlayerFromQueue(currentPlayer);
             gui.removePlayerFromGUI(board);
         }
-
     }
 
     public void removePlayerFromQueue(Player player) {
@@ -201,84 +199,91 @@ public class Monopoly extends JPanel {
     public void createSquare(Board board) {
         Start Go = new Start();
         board.getSquares().add(Go);
-        City HongKong = new City("Hong Kong", 2, 50000, 0, 50000);
-        board.getSquares().add(HongKong);
-        City Beijing = new City("Beijing", 3, 52000, 0, 50000);
-        board.getSquares().add(Beijing);
-        City BangKok = new City("BangKok", 4, 55000, 0, 50000);
-        board.getSquares().add(BangKok);
-        City LonDon = new City("London", 5, 60000, 0, 50000);
-        board.getSquares().add(LonDon);
-        Beach Bali = new Beach("Bali", 6, 50000);
-        board.getSquares().add(Bali);
-        City Singapore = new City("Singapore", 7, 65000, 0, 50000);
-        board.getSquares().add(Singapore);
-        City Paris = new City("Paris", 8, 70000, 0, 50000);
-        board.getSquares().add(Paris);
-        City Dubai = new City("Dubai", 9, 75000, 0, 50000);
-        board.getSquares().add(Dubai);
-        City NewYork = new City("NewYork", 10, 80000, 0, 50000);
-        board.getSquares().add(NewYork);
+
+        City DaLat = new City("Da Lat", 2, 50000, 0, 50000);
+        board.getSquares().add(DaLat);
+        City NhaTrang = new City("Nha Trang", 3, 52000, 0, 50000);
+        board.getSquares().add(NhaTrang);
+        City HaNoi = new City("Ha Noi", 4, 55000, 0, 50000);
+        board.getSquares().add(HaNoi);
+        City HoChiMinh = new City("Ho Chi Minh", 5, 60000, 0, 50000);
+        board.getSquares().add(HoChiMinh);
+        Card chance1 = new Card("Chance", 6);
+        board.getSquares().add(chance1);
+        City Seoul = new City("Seoul", 7, 65000, 0, 50000);
+        board.getSquares().add(Seoul);
+        City Busan = new City("Busan", 8, 80000, 0, 50000);
+        board.getSquares().add(Busan);
+        City Incheon = new City("Incheon", 9, 80000, 0, 50000);
+        board.getSquares().add(Incheon);
+        Beach Fiji = new Beach("Fiji", 10, 50000);
+        board.getSquares().add(Fiji);
+
         Jail jail = new Jail();
         board.getSquares().add(jail);
-        City KualaLumpur = new City("Kuala Lumpur", 12, 100000, 0, 70000);
-        board.getSquares().add(KualaLumpur);
-        City Istanbul = new City("Istanbul", 13, 110000, 0, 70000);
-        board.getSquares().add(Istanbul);
-        City Delhi = new City("Delhi", 14, 120000, 0, 70000);
-        board.getSquares().add(Delhi);
-        Card chance1 = new Card("Chance", 15);
-        board.getSquares().add(chance1);
-        City Mumbai = new City("Mumbai", 16, 130000, 0, 70000);
-        board.getSquares().add(Mumbai);
-        Beach Maldives = new Beach("Maldives", 17, 50000);
-        board.getSquares().add(Maldives);
-        City Rome = new City("Rome", 18, 140000, 0, 70000);
-        board.getSquares().add(Rome);
-        City SaoPaulo = new City("Sao Paulo", 19, 150000, 0, 70000);
-        board.getSquares().add(SaoPaulo);
-        City Taipei = new City("Taipei", 20, 160000, 00, 70000);
-        board.getSquares().add(Taipei);
+
+        City Tokyo = new City("Tokyo", 12, 100000, 0, 70000);
+        board.getSquares().add(Tokyo);
+        City Kyoto = new City("Kyoto", 13, 110000, 0, 70000);
+        board.getSquares().add(Kyoto);
+        City Osaka = new City("Osaka", 14, 120000, 0, 70000);
+        board.getSquares().add(Osaka);
+        Company Electrical = new Company("Electrical", 15, 50000);
+        board.getSquares().add(Electrical);
+        Card chance2 = new Card("Chance", 16);
+        board.getSquares().add(chance2);
+        City Cairo = new City("Cairo", 17, 130000, 0, 70000);
+        board.getSquares().add(Cairo);
+        City Nairobi = new City("Nairobi", 18, 140000, 0, 70000);
+        board.getSquares().add(Nairobi);
+        City Marrakech = new City("Marrakech", 19, 150000, 0, 70000);
+        board.getSquares().add(Marrakech);
+        Beach Bahamas = new Beach("Bahamas", 20, 50000);
+        board.getSquares().add(Bahamas);
+
         Event event = new Event("World Cup", 21, SquareType.EVENT);
         board.getSquares().add(event);
-        City Seoul = new City("Seoul", 22, 170000, 0, 80000);
-        board.getSquares().add(Seoul);
-        Beach HaLong = new Beach("Ha Long Bay", 23, 50000);
-        board.getSquares().add(HaLong);
-        City Barcelona = new City("Barcelona", 24, 185000, 0, 80000);
+
+        City Paris = new City("Paris", 22, 170000, 0, 80000);
+        board.getSquares().add(Paris);
+        City London = new City("London", 23, 185000, 0, 80000);
+        board.getSquares().add(London);
+        City Rome = new City("Rome", 24, 200000, 0, 80000);
+        board.getSquares().add(Rome);
+        City Barcelona = new City("Barcelona", 25, 215000, 0, 80000);
         board.getSquares().add(Barcelona);
-        City LosAngles = new City("LosAngles", 25, 200000, 0, 80000);
-        board.getSquares().add(LosAngles);
-        Card chance2 = new Card("Chance", 26);
-        board.getSquares().add(chance2);
-        City HaNoi = new City("Ha Noi", 27, 215000, 0, 80000);
-        board.getSquares().add(HaNoi);
-        City Milan = new City("Milan", 28, 230000, 0, 80000);
-        board.getSquares().add(Milan);
-        City Berlin = new City("Berlin", 29, 245000, 0, 80000);
+        Card chance3 = new Card("Chance", 26);
+        board.getSquares().add(chance3);
+        City Berlin = new City("Berlin", 27, 230000, 0, 80000);
         board.getSquares().add(Berlin);
-        City MosCow = new City("Moscow", 30, 260000, 0, 80000);
-        board.getSquares().add(MosCow);
+        City Amsterdam = new City("Amsterdam", 28, 245000, 0, 80000);
+        board.getSquares().add(Amsterdam);
+        City Venice = new City("Venice", 29, 260000, 0, 80000);
+        board.getSquares().add(Venice);
+        Beach Prague = new Beach("Prague", 30, 50000);
+        board.getSquares().add(Prague);
+
         Plane plane = new Plane();
         board.getSquares().add(plane);
-        Beach Miami = new Beach("Miami", 32, 50000);
-        board.getSquares().add(Miami);
-        City Sydney = new City("Sydney", 33, 270000, 0, 100000);
-        board.getSquares().add(Sydney);
-        Tax tax = new Tax();
-        board.getSquares().add(tax);
+
+        Company Water = new Company("Water", 32, 50000);
+        board.getSquares().add(Water);
+        City Shenzhen = new City("Shenzhen", 33, 270000, 0, 100000);
+        board.getSquares().add(Shenzhen);
+        City Guangzhou = new City("Guangzhou", 34, 270000, 0, 100000);
+        board.getSquares().add(Guangzhou);
         City Shanghai = new City("Shanghai", 35, 270000, 0, 100000);
         board.getSquares().add(Shanghai);
-        Card chance3 = new Card("Chance", 36);
-        board.getSquares().add(chance3);
-        City Madrid = new City("Madrid", 37, 300000, 0, 100000);
-        board.getSquares().add(Madrid);
-        City Venice = new City("Venice", 38, 330000, 0, 100000);
-        board.getSquares().add(Venice);
-        City Osaka = new City("Osaka", 39, 360000, 0, 100000);
-        board.getSquares().add(Osaka);
-        City Tokyo = new City("Tokyo", 40, 360000, 0, 100000);
-        board.getSquares().add(Tokyo);
+        City Beijing = new City("Beijing", 36, 300000, 0, 100000);
+        board.getSquares().add(Beijing);
+        Tax tax = new Tax();
+        board.getSquares().add(tax);
+        City LosAngeles = new City("Los Angeles", 38, 360000, 0, 100000);
+        board.getSquares().add(LosAngeles);
+        City NewYork = new City("New York", 39, 360000, 0, 100000);
+        board.getSquares().add(NewYork);
+        Beach Maldives = new Beach("Maldives", 40, 50000);
+        board.getSquares().add(Maldives);
     }
 
     public void printResult(Board board) {
@@ -299,7 +304,5 @@ public class Monopoly extends JPanel {
             int totalMoney = player.getCash() + player.calculateTotalPropertyValue();
             System.out.println("Player " + player.getName() + ": $" + totalMoney);
         }
-
-        // In ra bang ket qua tong so tien cua cac player sau round cuoi cung
     }
 }
